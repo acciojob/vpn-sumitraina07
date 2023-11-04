c0e049455d41046be4a6beb877c05d8a3421da2d
sumeetraina <sumitraina07@gmail.com>
2023-11-04 19:23:44 +0530

    Second Commit

--- src/main/java/com/driver/model/Connection.java
+++ src/main/java/com/driver/model/Connection.java
@@ -43,7 +43,7 @@ public class Connection {
     }
 
     public ServiceProvider getServiceProvider() {
-        return serviceProvider;
+        return serviceProvide;
     }
 
     public void setServiceProvider(ServiceProvider serviceProvider) {
